b5f35868ca0ca90ef2e1cc2ffaa529775516db6c
David Wilsher (s5067869) <s5067869@bournemouth.ac.uk>
2019-10-04 14:56:03 +0100

    first commit

---
+++ Build/CMakeFiles/Big_Boy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Big_Boy.lib"
+  "Big_Boy.pdb"
+  "CMakeFiles/Big_Boy.dir/src/Big_Boy/dummy.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Big_Boy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
